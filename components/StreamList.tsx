ddb68347da5cb0a6c46e5d0337acb47697438539
Michael Lynch <mplynch2020@gmail.com>
2024-12-31 20:38:29 +0000

    Added a component that shows a list of all the streams

---
+++ components/StreamList.tsx
@@ -0,0 +1,53 @@
+'use client'
+import React from "react";
+
+function StreamList() {
+    const [streams, setStreams] = React.useState<(string | null)[]>([]);  
+    React.useEffect(() => {
+      fetch("http://5.75.179.226:8080/stat")
+        .then(resp => resp.text())
+        .then(xmlStr => {
+          const parser = new DOMParser();
+          const doc = parser.parseFromString(xmlStr, "text/xml");
+          console.log("Raw XML:", xmlStr);
+
+          const streamNames: (string | null)[] = [];
+          const applications = doc.getElementsByTagName("application");
+          console.log("Found applications:", applications.length);
+          
+          for (let i = 0; i < applications.length; i++) {
+            const app = applications[i];
+            const liveElements = app.getElementsByTagName("live");
+            
+            for (let j = 0; j < liveElements.length; j++) {
+              const live = liveElements[j];
+              const streams = live.getElementsByTagName("stream");
+              
+              for (let k = 0; k < streams.length; k++) {
+                const stream = streams[k];
+                const nameElement = stream.getElementsByTagName("name")[0];
+                if (nameElement && nameElement.textContent) {
+                  streamNames.push(nameElement.textContent);
+                }
+              }
+            }
+          }
+          
+          setStreams(streamNames);
+        })
+        .catch(error => {
+          console.error("Error fetching streams:", error);
+        });
+    }, []);
+  
+    return (
+      <ul>
+        {streams.map(name => (
+          <li key={name}>{name}</li>
+        ))}
+      </ul>
+    );
+  }
+  
+  export default StreamList;
+  
---
+++ components/index.ts
@@ -0,0 +1,1 @@
+export { default as StreamList } from './StreamList'
