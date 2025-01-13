4cf615c4b1a0e688c47936540953759a17ab16fb
Michael Lynch <mplynch2020@gmail.com>
2025-01-13 01:42:55 +0000

    Fixed the bug of blank space, but a bit hacky because I have almost hardcoded it by saying only have the ml-64 and sidebar if screen size is large. Would maybe be better if the relationship between needing the extra space and having the sidebar was a bit more explicit, by extracting the logic into some config

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,12 +32,15 @@ export default function RootLayout({
       >
         <UserProvider>
           <div className="flex min-h-screen">
-            <div className="fixed top-0 left-0 w-64 h-full z-50 bg-gray-900">
+            <div className="fixed top-0 left-0 w-full h-16 z-50 bg-gray-900">
               <Navbar />
+            </div>
+
+            <div className="hidden lg:block fixed top-16 left-0 w-64 h-full z-40 bg-gray-900">
               <RecommendedChannels />
             </div>
 
-            <div className="ml-64 flex-1">
+            <div className="lg:ml-64 flex-1">
               <div className="mt-16">{children}</div>
             </div>
           </div>
