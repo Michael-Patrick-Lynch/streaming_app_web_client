7cd782e7395b0cb53d106ad9e3a63736c769df99
Michael Lynch <mplynch2020@gmail.com>
2025-02-07 16:09:29 +0000

    NewNavbar.tsx is now named Navbar.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import { Navbar } from '@/components/NewNavbar';
+import { Navbar } from '@/components/Navbar';
 import { UserProvider } from '@/context/UserContext';
 
 const geistSans = Geist({
