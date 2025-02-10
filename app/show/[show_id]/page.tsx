aacd2b7c0f6c56c048679e002f69fc39c213581a
Michael Lynch <mplynch2020@gmail.com>
2025-02-10 01:37:50 +0000

    Formatting and containers on WatchShowPage

--- app/show/[show_id]/page.tsx
+++ app/show/[show_id]/page.tsx
@@ -4,12 +4,27 @@ import LiveStream from '@/components/LiveStream';
 
 export default function WatchShowPage() {
   return (
-    <div className="w-full h-full lg:w-1/3">
-      <LiveStream
-        streamUrl="https://api.firmsnap.com/hls/bullshit.m3u8"
-        streamerName="FashionShopLive"
-        streamerImage="/profile.jpg"
-      />
+    <div style={{ height: 'calc(100vh - 64px)' }} className="overflow-hidden">
+      <div className="flex flex-col lg:flex-row w-full h-full">
+        {/* Shop (Desktop Only) */}
+        <div className="hidden lg:flex lg:w-1/3 items-center justify-center bg-gray-100 p-4">
+          <p>Shop</p>
+        </div>
+
+        {/* LiveStream Component (chat will be in that component on mobile) */}
+        <div className="w-full h-full lg:w-1/3">
+          <LiveStream
+            streamUrl="https://api.firmsnap.com/hls/bullshit.m3u8"
+            streamerName="FashionShopLive"
+            streamerImage="/profile.jpg"
+          />
+        </div>
+
+        {/* Livechat (Desktop Only) */}
+        <div className="hidden lg:flex lg:w-1/3 items-center justify-center bg-gray-100 p-4">
+          <p>Livechat</p>
+        </div>
+      </div>
     </div>
   );
 }
