b006d418b727ed4776dc10da0be0ae3c5dcfed53
Michael Lynch <mplynch2020@gmail.com>
2025-02-18 05:00:27 +0000

    Charging buyers a different amount of shipping if they are EU wide or domestic, taking shipping details, and only allowing EU countries to buy on the app (Stripe checkout)

--- app/api/stripe/create_checkout_session/route.ts
+++ app/api/stripe/create_checkout_session/route.ts
@@ -1,13 +1,18 @@
 // This API needs to live in Next because
 // of CORS issues around redirects
 import { stripe } from '@/lib/utils';
+import type { Stripe } from 'stripe';
+type AllowedCountry =
+  Stripe.Checkout.Session.ShippingAddressCollection.AllowedCountry;
 import { NextResponse } from 'next/server';
+import { Listing } from '@/components/Shop';
 
 export async function POST(req: Request) {
   const formData = await req.formData();
   const listingId = parseInt(formData.get('listingId') as string, 10);
   const quantity = parseInt(formData.get('quantity') as string, 10);
   const token = formData.get('token') as string;
+  const buyerCountry = formData.get('buyerCountry') as string;
 
   // Reserve inventory
   const reserveRes = await fetch('https://api.firmsnap.com/reservations', {
@@ -30,8 +35,33 @@ export async function POST(req: Request) {
       `https://api.firmsnap.com/listings/${listingId}`
     );
     const listing = await listingRes.json();
+    console.log('Listing:', listing);
+
+    const shippingDetails = createShippingDetails(buyerCountry, listing);
 
     const session = await stripe.checkout.sessions.create({
+      shipping_address_collection: {
+        allowed_countries:
+          shippingDetails.shipping_address_collection.allowed_countries,
+      },
+      shipping_options: [
+        {
+          shipping_rate_data: {
+            type: 'fixed_amount',
+            fixed_amount: {
+              amount:
+                shippingDetails.shipping_options.shipping_options
+                  .shipping_rate_data.fixed_amount.amount,
+              currency:
+                shippingDetails.shipping_options.shipping_options
+                  .shipping_rate_data.fixed_amount.currency,
+            },
+            display_name:
+              shippingDetails.shipping_options.shipping_options
+                .shipping_rate_data.display_name,
+          },
+        },
+      ],
       line_items: [
         {
           price_data: {
@@ -63,4 +93,124 @@ export async function POST(req: Request) {
     );
     return new Response(`Internal Server Error: ${error}`, { status: 500 });
   }
+}
+
+// Function to generate shipping details
+const createShippingDetails = (buyerCountry: string, listing: Listing) => {
+  if (buyerCountry === listing.seller_country) {
+    console.log(
+      `Buyer country: ${buyerCountry}, seller country: ${listing.seller_country}`
+    );
+    return {
+      shipping_address_collection: {
+        allowed_countries: [countryToIsoCode(listing.seller_country)],
+      },
+      shipping_options: buildShippingOptions(
+        listing.shipping_domestic_price,
+        `Domestic Shipping Within ${listing.seller_country}`
+      ),
+    };
+  } else {
+    console.log(
+      `Buyer country: ${buyerCountry}, seller country: ${listing.seller_country}`
+    );
+    console.log(`Listing object: ${listing}`);
+    return {
+      shipping_address_collection: {
+        allowed_countries: allEuIsoCodes(),
+      },
+      shipping_options: buildShippingOptions(
+        listing.shipping_eu_price,
+        'EU Shipping'
+      ),
+    };
+  }
+};
+
+function buildShippingOptions(
+  shipping_cost: { amount: number; currency: string },
+  shipping_name: string
+) {
+  return {
+    shipping_options: {
+      shipping_rate_data: {
+        type: 'fixed_amount',
+        fixed_amount: {
+          amount: shipping_cost.amount,
+          currency: shipping_cost.currency.toLowerCase(),
+        },
+        display_name: shipping_name,
+      },
+    },
+  };
+}
+
+function countryToIsoCode(country: string): AllowedCountry {
+  const mapping: Record<string, AllowedCountry> = {
+    Austria: 'AT',
+    Belgium: 'BE',
+    Bulgaria: 'BG',
+    Croatia: 'HR',
+    Cyprus: 'CY',
+    'Czech Republic': 'CZ',
+    Denmark: 'DK',
+    Estonia: 'EE',
+    Finland: 'FI',
+    France: 'FR',
+    Germany: 'DE',
+    Greece: 'GR',
+    Hungary: 'HU',
+    Ireland: 'IE',
+    Italy: 'IT',
+    Latvia: 'LV',
+    Lithuania: 'LT',
+    Luxembourg: 'LU',
+    Malta: 'MT',
+    Netherlands: 'NL',
+    Poland: 'PL',
+    Portugal: 'PT',
+    Romania: 'RO',
+    Slovakia: 'SK',
+    Slovenia: 'SI',
+    Spain: 'ES',
+    Sweden: 'SE',
+  };
+
+  const isoCode = mapping[country];
+  if (!isoCode) {
+    throw new Error(`Invalid country: ${country}`);
+  }
+  return isoCode;
+}
+
+function allEuIsoCodes(): AllowedCountry[] {
+  return [
+    'AT', // Austria
+    'BE', // Belgium
+    'BG', // Bulgaria
+    'HR', // Croatia
+    'CY', // Cyprus
+    'CZ', // Czech Republic
+    'DK', // Denmark
+    'EE', // Estonia
+    'FI', // Finland
+    'FR', // France
+    'DE', // Germany
+    'GR', // Greece
+    'HU', // Hungary
+    'IE', // Ireland
+    'IT', // Italy
+    'LV', // Latvia
+    'LT', // Lithuania
+    'LU', // Luxembourg
+    'MT', // Malta
+    'NL', // Netherlands
+    'PL', // Poland
+    'PT', // Portugal
+    'RO', // Romania
+    'SK', // Slovakia
+    'SI', // Slovenia
+    'ES', // Spain
+    'SE', // Sweden
+  ];
 }
